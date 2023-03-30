99968c115e3107f12a322129fdc896c1cc92063f
root <mbakulin103@gmail.com>
2023-03-30 09:17:55 -0400

    Add_dictionary_mac_and_ip_address

--- network_scanner.py
+++ network_scanner.py
@@ -9,8 +9,12 @@ def scan(ip):
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
     print("IP\t\t\tMAC address\n-----------------------------------------")
+    clients_list = []
 
     for element in answered_list:
+        clients_dict = {'ip': element[1].psrc, 'mac': element[1].hwsrc}
+        clients_list.append(clients_dict)
         print(element[1].psrc + "\t\t" + element[1].hwsrc)
+    print(clients_list)
 
 scan('192.168.0.1/24')
